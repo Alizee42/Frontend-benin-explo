9568dba72ce475e83501f4f0f113b35c6ef35462
Alizée GUEYE <AG@def-systemes.local>
2025-11-24 16:58:48 +0100

    Ajout page details circuits

--- src/app/features/circuits/pages/circuit-detail/circuit-detail.component.ts
+++ src/app/features/circuits/pages/circuit-detail/circuit-detail.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-circuit-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './circuit-detail.component.html',
-  styleUrl: './circuit-detail.component.scss'
+  styleUrls: ['./circuit-detail.component.scss'],
 })
 export class CircuitDetailComponent {
+  circuit = {
+    id: 1,
+    titre: "Circuit Porto-Novo Tour",
+    duree: "1 journée",
+    localisation: "Porto-Novo — Département de l’Ouemé",
+    resume:
+      "Une journée immersive entre lieux Vodoun, musées, nature et balade en pirogue. Un circuit culturel et authentique pour découvrir Porto-Novo autrement.",
+    img: "https://images.unsplash.com/photo-1526779248011-7daef7c3e9f4",
 
+    tourisme: [
+      "Centre culturel Ouadada",
+      "Places Vodoun (Dagbé, Lokossa, Abessan, Yedomin, etc.)",
+      "Couvents Vodoun (Sakpata, Gbeloko)",
+      "Place Migan",
+      "Jardin des plantes & nature",
+      "Musée Adandé",
+      "Musée Honmè",
+    ],
+
+    aventures: [
+      "Chasse au trésor — Sentinelle du climat",
+      "Découverte du Vodoun Houngbo Sakpata",
+      "Tour de la capitale",
+      "Balade Rivière Noire",
+      "Village lacustre des Aguégués",
+      "Potières de Ouémé",
+    ],
+
+    programme: [
+      "Centre culturel Ouadada",
+      "Place Migan",
+      "Place Vodoun Adjinan",
+      "Jardin des plantes",
+      "Tour de la capitale",
+      "Pirogue – Rivière noire",
+    ],
+  };
 }
